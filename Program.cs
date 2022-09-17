56df5d3372b0d461d62f45b4cd586fd6e2bd5c1f
helder <jose.silva2000@etec.sp.gov.br>
2022-09-17 09:26:06 -0300

    resolução da atividade feita sobre nome completo

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("digite seu nome : ");
+string nomeDigitado = Console.ReadLine()!;
+Console.Write("digite seu sobrenome : ");
+string sobreNome = Console.ReadLine()!;
+Console.Write($"olá ,{nomeDigitado}{sobreNome}!");
